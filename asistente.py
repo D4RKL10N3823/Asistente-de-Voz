df0fcd40ba765aa105ca1cb63e39325e81cfe4ef
D4RKL10N3823 <88405570+D4RKL10N3823@users.noreply.github.com>
2024-03-27 11:01:21 -0600

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -6,72 +6,47 @@ import datetime
 import pyautogui
 import time
 
-# opciones de voz / idioma
 id1 = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0'
 
-# escuchar nuestro microfono y devolver el audio comotexto
 def trasformar_audio_en_texto():
-    # almacenar recognizer en variable
     microfono = sr.Recognizer()
 
-    # configurar el microfono
     with sr.Microphone() as origen:
-        # tiempo de espera
         microfono.pause_threshold = 0.8
-        # informar que comenzo la grabacion
         print("ya puedes hablar")
-        # guardar lo que escuche como audio
         audio =microfono.listen(origen)
 
         try:
-            # buscar en google
             pedido = microfono.recognize_google(audio, language="es-mx")
-            # prueba de que pudo ingresar
             print(f"Dijiste: {pedido}")
-            # devolver pedido
             return pedido
 
-        # en caso de que no comprenda el audio
         except sr.UnknownValueError:
-            # prueba de que no comprendio el audio
             print("ups, no entendi")
-            # devolver error
             return "sigo esperando"
 
-        # en caso de no resolver el pedido
         except sr.RequestError:
-            # prueba de que no comprendio el audio
             print("ups, no hay servicio")
-            # devolver error
             return "sigo esperando"
 
-        # error inesperado
         except:
-            # prueba de que no comprendio el audio
             print("ups, algo ha salido mal")
-            # devolver error
             return "sigo esperando"
 
 
-# Inicializar el motor de texto a voz (pyttsx3) una sola vez al comienzo del programa
 engine = pyttsx3.init()
 engine.setProperty('voice', id1)
-# Función para que el asistente pueda ser escuchado
+
 def hablar(mensaje):
-    # Pronunciar mensaje
     engine.say(mensaje)
     engine.runAndWait()
 
 
-# informar el dia de la semana
 def pedir_dia():
-    # crear variable con datos de hoy
     dia = datetime.date.today()
     print(dia)
-    # crear variable para el dia de semana
     dia_semana = dia.weekday()
     print(dia_semana)
-    # diccionario con nombres de dias
     calendario = {0: 'Lunes',
                   1: 'Martes',
                   2: 'Miércoles',
@@ -79,17 +54,14 @@ def pedir_dia():
                   4: 'Viernes',
                   5: 'Sábado',
                   6: 'Domingo'}
-    # decir el dia de la semana
     hablar(f'Hoy es {calendario[dia_semana]}')
 
 
-# informar que hora es
 def pedir_hora():
-    # crear una variab;e con datos de la hora
     hora = datetime.datetime.now()
     hora = f'En este momento son las {hora.hour} horas con {hora.minute} minutos y {hora.second} segundos'
     print(hora)
-    # decir la hora
+    
     hablar(hora)
 
 def pedir_nombre():
@@ -98,11 +70,9 @@ def pedir_nombre():
     return nombre
 
 
-# funcion saludo inicial
 def saludo_inicial():
     nombre = pedir_nombre()
 
-    # crear variable condatos de hora
     hora = datetime.datetime.now()
     if hora.hour < 6 or hora.hour > 20:
         momento = 'Buenas noches'
@@ -110,7 +80,6 @@ def saludo_inicial():
         momento = 'Buen día'
     else:
         momento = 'Buenas tardes'
-    # decir el saludo
     hablar(f'{momento} {nombre}, dime en qué te puedo ayudar')
 
 
@@ -173,9 +142,7 @@ def escribir_nota():
 def abrir_sitio_web(url, mensaje):
     hablar(mensaje)
     webbrowser.open(url)
-
-
-# funcion central del asistente
+    
 def pedir_cosas():
     saludo_inicial()
 
@@ -186,7 +153,6 @@ def pedir_cosas():
             hablar("Me voy a descansar, cualquier cosa me avisas")
             break
 
-        # Utilizar un diccionario para mapear comandos a funciones podría simplificar esta estructura
         acciones = {
             'abrir youtube': lambda: abrir_sitio_web('https://www.youtube.com', 'Con gusto, estoy abriendo YouTube'),
             'busca en internet': lambda pedido=pedido: buscar_en_internet(pedido),
